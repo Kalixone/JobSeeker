a769075030552e72e567432e7f122450877ceed9
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-12-11 21:26:06 +0100

    added globalExceptionHandler

---
+++ src/main/java/com/cvenjoyer/cv_enjoyer/exceptions/CustomErrorResponse.java
@@ -0,0 +1,14 @@
+package com.cvenjoyer.cv_enjoyer.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record CustomErrorResponse(
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        List<String> errors
+) {
+}
